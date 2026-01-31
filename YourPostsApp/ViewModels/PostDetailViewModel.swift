c08ac1842c7e909329a5595d660b8219370bf106
kurog1ri <kosenko.yosiki@gmail.com>
2026-01-31 18:51:18 +0200

    feat: add PostDetailViewModel

---
+++ YourPostsApp/ViewModels/PostDetailViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  PostDetailViewModel.swift
+//  YourPostsApp
+//
+
+import Foundation
+
+final class PostDetailViewModel {
+
+    private let networkService = NetworkService()
+    private let postId: Int
+
+    private(set) var postDetail: PostDetail?
+
+    init(postId: Int) {
+        self.postId = postId
+    }
+
+    func fetchPostDetail() async throws {
+        postDetail = try await networkService.fetchPostDetail(id: postId)
+    }
+
+    var formattedDate: String {
+        guard let timestamp = postDetail?.timeshamp else { return "" }
+
+        let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, yyyy"
+
+        return formatter.string(from: date)
+    }
+
+    var formattedLikes: String {
+        guard let likes = postDetail?.likesCount else { return "" }
+        return "\(likes) likes"
+    }
+}
