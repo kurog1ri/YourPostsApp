403377d3e35b48a39b8d968d2abcefef5074a285
kurog1ri <kosenko.yosiki@gmail.com>
2026-01-31 18:31:25 +0200

    feat: add Post models

---
+++ YourPostsApp/Models/Post.swift
@@ -0,0 +1,52 @@
+//
+//  Post.swift
+//  YourPostsApp
+//
+
+import Foundation
+
+// MARK: - Feed Response
+
+struct PostsResponse: Codable {
+    let posts: [Post]
+}
+
+struct Post: Codable {
+    let postId: Int
+    let timeshamp: Int 
+    let title: String
+    let previewText: String
+    let likesCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case postId
+        case timeshamp
+        case title
+        case previewText = "preview_text"
+        case likesCount = "likes_count"
+    }
+}
+
+// MARK: - Detail Response
+
+struct PostDetailResponse: Codable {
+    let post: PostDetail
+}
+
+struct PostDetail: Codable {
+    let postId: Int
+    let timeshamp: Int
+    let title: String
+    let text: String
+    let postImage: String
+    let likesCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case postId
+        case timeshamp
+        case title
+        case text
+        case postImage
+        case likesCount = "likes_count"
+    }
+}
