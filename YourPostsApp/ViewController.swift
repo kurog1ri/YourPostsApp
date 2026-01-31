6315aa5a0014ea35ab4798c1fc138f8fb82026b8
kurog1ri <kosenko.yosiki@gmail.com>
2026-01-31 17:05:41 +0200

    Initial Commit

---
+++ YourPostsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YourPostsApp
+//
+//  Created by   Kosenko Mykola on 31.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
